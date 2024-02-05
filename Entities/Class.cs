ffdc7e198d8a6280c352c6b23ee7d1c750fc0372
wnx82 <vandermeulen.christophe@gmail.com>
2024-02-05 20:51:21 +0100

    Création de l'entité

---
+++ Entities/Class.cs
@@ -0,0 +1,19 @@
+﻿namespace Filmotech.Entities
+{
+    public class Films
+    {
+        public Guid? Id{ get; set; }
+
+        public string? Titre { get; set; }
+
+        public string? Qualité { get; set; }
+
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public DateTime? DeletedAt { get; set; }
+
+
+
+
+    }
+}
